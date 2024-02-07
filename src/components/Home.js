11712cc6ac1a3a29cd6d047cac9459d49e1a09f9
Parishlesh <parishleshfulvanshi@gmail.com>
2024-02-07 17:29:40 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../styles/home.css'
+
+export default function Home() {
+  return (
+    <div className='cardContainer'>
+      <div className="card">
+        <img src='photo1.jpg'></img>
+        hellow
+      </div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">RoomRover</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarScroll">
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" styles={"--bs-scroll-height: 100px;"}>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search Location" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
