0dadadaad72530138ceb9d2ed7ca039730588246
Parishlesh <parishleshfulvanshi@gmail.com>
2024-02-07 18:02:59 +0530

    ...

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,56 @@
 import React from 'react'
 import styles from '../styles/home.css'
 
+
+
 export default function Home() {
   return (
     <div className='cardContainer'>
       <div className="card">
         <img src='photo1.jpg'></img>
-        hellow
+        <hr></hr>
+        <div className='details'>
+        <span><b>Rs. 4000</b></span>
+        <span>data</span>
+        <span>location data</span>
+        </div>
       </div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
-      <div className="card"></div>
+      <div className="card">
+        <img src='photo1.jpg'></img>
+        <hr></hr>
+        <div className='details'>
+        <span><b>Rs. 4000</b></span>
+        <span>data</span>
+        <span>location data</span>
+        </div>
+      </div>
+      <div className="card">
+        <img src='photo1.jpg'></img>
+        <hr></hr>
+        <div className='details'>
+        <span><b>Rs. 4000</b></span>
+        <span>data</span>
+        <span>location data</span>
+        </div>
+      </div>
+      <div className="card">
+        <img src='photo1.jpg'></img>
+        <hr></hr>
+        <div className='details'>
+        <span><b>Rs. 4000</b></span>
+        <span>data</span>
+        <span>location data</span>
+        </div>
+      </div>
+      <div className="card">
+        <img src='photo1.jpg'></img>
+        <hr></hr>
+        <div className='details'>
+        <span><b>Rs. 4000</b></span>
+        <span>data</span>
+        <span>location data</span>
+        </div>
+      </div>
     </div>
   )
 }
