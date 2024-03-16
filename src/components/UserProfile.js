a8040a5eb8e7d8fe07e6853870fd0b573a56abed
Parishlesh <parishleshfulvanshi@gmail.com>
2024-03-17 01:47:04 +0530

    ...

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -10,11 +10,13 @@ const UserProfile = () => {
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col col-lg-11 col-xl-101">
                         <div className="userProfileCard">
-                            <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
-                                <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                                    <img src={profileimg}
-                                        alt="Generic placeholder image" className="img-fluid img-thumbnail mt-4 mb-2 "
-                                        style={{ width: '150px', zIndex: 1 }} />
+                            <div className="rounded text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
+                                <div className="ms-4  d-flex flex-column" style={{ width: '150px' }}>
+                                    <div className="profileimg" style={{ width: '150px', height: '150px', overflow: 'hidden' }}>
+                                        <img src={profileimg}
+                                            alt="Generic placeholder image" className="img-fluid img-thumbnail mt-4 mb-2 "
+                                            style={{ width: '150px', zIndex: 1 }} />
+                                    </div>
                                     <button type="button" className="btn btn-outline-dark" data-mdb-ripple-color="dark"
                                         style={{ zIndex: 1 }}>
                                         Edit profile
@@ -25,8 +27,7 @@ const UserProfile = () => {
                                     <p>Hisar</p>
                                 </div>
                             </div>
-                            <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
-                            </div>
+                          
                             <div className="card-body p-4 text-black">
                                 <div className="mb-5">
                                     <p className="lead fw-normal mb-1">About</p>
